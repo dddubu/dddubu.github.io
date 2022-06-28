75d15d31ebcb651b8328e660f41cfb983ef5ab35
Tyler Kim <106580580+dddubu@users.noreply.github.com>
2022-06-28 16:36:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
-
-
 let dataServer;
 let pubKey = "pub-c-c0459111-434c-403a-980e-2eff7fc61d04";
 let subKey = "sub-c-e1f3336e-ce21-49d5-a2da-29da7dcd2741";
 let secretKey = "sec-c-ZTgyOGY0YTktYThjYS00MzBiLWIwNDItMzM2ZjEwNjJkM2Ex";
 
-let occupancy = 0; 
+// channel name is the "room" that you will all be chatting in (think about it like a group chat)
+let channelName = "race";
+let cursors = [];
 
-let channelName = "presenceTest";
+let x = (100,100)
+let y = (100,100)
+let you; 
 
-let allowMessage = false;
-let you;
+let refresh = 100;
+let lastRefresh = 0;
+
 
 function preload() {
 
@@ -20,92 +23,111 @@ function preload() {
   console.log(you);
   you = you.toString();
 }
-  
-  
+
 function setup() {
-
-    createCanvas(windowWidth, windowHeight);
- 
-    dataServer = new PubNub({
-      subscribeKey: subKey,
-      publishKey: pubKey,
-      uuid: you,
-      secretKey: secretKey,
-      heartbeatInterval: 0,
-    });
-
-     // listen for messages coming through the subcription feed on this specific channel. 
-
-    dataServer.subscribe({ channels: [channelName],   withPresence: true });
-    dataServer.addListener({ message: readIncoming, presence: whoisconnected });
-   
-  
-  }
-  
-function draw() {
- 
- // make something visible for more people 
-
-
- if (occupancy > 2) {
+  createCanvas(windowWidth, windowHeight);
   background(255);
 
+  // initialize our PubNub Server
+  dataServer = new PubNub({
+    subscribeKey: subKey,
+    publishKey: pubKey,
+    uuid: "you",
+    secretKey: "secretKey",
+    heartbeatInterval: 0,
+  });
 
-  textSize(20)
-  text("dubu </3 u", windowWidth/2, windowHeight/2);
+  // listen for messages coming through the subcription feed on this specific channel. 
+  dataServer.addListener({ message: readIncoming});
+  dataServer.subscribe({ channels: [channelName] });
 
-
-  textSize(200)
-  text("</3",windowWidth/3, windowHeight/2);
-
-  allowMessage = false;
-
- } else if (occupancy > 1) {
-
-  sendTheMessage();
-  allowMessage = true;
-  
- } else {
-  background(255);
-
-  textSize(20);
-  text("dubu <3 u", windowWidth/2, windowHeight/2); 
-  
-  textSize(200)
-  text("<3",windowWidth/3, windowHeight/2);
-  allowmessage = false;
-
-  }
+  // create a new JSON object to store our data
+  new allCursors(mouseX,mouseY,dataServer.getUUID())
 }
-  // PubNub logic below
+
+function draw() {
+
+  background(255);
+
+  ellipse(x,y,20,20);
+  
+
+
+  
+sendTheMessage(); // send the message with the cursor location every 100ms. 
+   
+  }
+
+  for (let i = 0; i < cursors.length; i++) { // loop through all the cursors and show them on the page
+    stroke(0);
+    strokeWeight(1);
+    // draw a small crosshair
+    line(cursors[i].x,cursors[i].y-5,cursors[i].x,cursors[i].y+5);
+    line(cursors[i].x-5,cursors[i].y,cursors[i].x+5,cursors[i].y);
+    }
+
+
+function mouseClicked(){
+  if ((mouseX > x-10) && (mouseX < x+20) &&
+  (mouseY > y-10) && (mouseY < y+20)) {
+  //background(255);
+   fill(240, 20, 140);
+} else {
+  fill(128);
+}
+
+}
+
+
+
+
+
+/// PubNub logic below
 function sendTheMessage() {
   // Send Data to the server to draw it in all other canvases
   dataServer.publish({
     channel: channelName,
     message: {
       x: mouseX,
-      y: mouseY
+      y: mouseY,
     },
   });
 }
 
 function readIncoming(inMessage) {
+  // when new data comes in it triggers this function,
+  // we call this function in the setup
 
-  if (allowMessage == true) { // if there is less than 10 people on the page draw circles then show the messages that are sent. 
+  /*since an App can have many channels, we ensure that we are listening
+  to the correct channel */
+
+  if (inMessage.channel == channelName) {
+
+   x = inMessage.message.x // get the mouseX value from the other people
+   y = inMessage.message.y // get the mouseY value from the other people
+   who = inMessage.publisher; // who sent the message
+
+ //  console.log(inMessage); //logging for information
+
+   let newinput = true; // we are checking to see if this person who sent the message is already on the page. 
+
+      for(let i = 0; i<cursors.length;i++) { // loop through all the IDs that have sent us messages before
+        if(you==cursors[i].you) { // if who is already in our array, update the x & y values
+          cursors[i].x = x;
+          cursors[i].y = y;
+          newinput = false;    // set the boolean to false since this is not a new user
+        }
+      }
+      if(newinput) { // if this is a new user, create a new JSON object that we add to our array
+        cursors.push(new allCursors(x,y, you));
+      }
+  }
+}
+function allCursors(x,y,you){ // creates a new JSON object for us
  
-    if (inMessage.channel == channelName) {
-        console.log(inMessage);
-    }
-
-    
-  } 
-}
-
-function whoisconnected(connectionInfo) {
-  console.log(connectionInfo);
-
-  occupancy = connectionInfo.occupancy;
-
-  console.log(occupancy);
+  this.x = x; // this is shorthand for saying "this object"
+  this.y = y;
+  this.you = you;
 
 }
+
